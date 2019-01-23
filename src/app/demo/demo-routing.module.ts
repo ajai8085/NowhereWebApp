8bd6fa19da3f0ae86db1c5665933b270b0f2be91
AJ <ajai.np@gmail.com>
2019-01-23 18:53:02 +1100

    Added second route flex

--- src/app/demo/demo-routing.module.ts
+++ src/app/demo/demo-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ButtonsComponent } from './buttons/buttons.component';
+import { FlexboxComponent } from './flexbox/flexbox.component';
 
 const routes: Routes = [{ path: 'button', component: ButtonsComponent },
+{ path: 'flexbox', component: FlexboxComponent },
 { path: '**', redirectTo: 'button' }];
 
 @NgModule({
--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -5,9 +5,10 @@ import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { MaterialModule } from '../shared/material/material.module';
 import { FormsModule } from '@angular/forms';
+import { FlexboxComponent } from './flexbox/flexbox.component';
 
 @NgModule({
-  declarations: [ButtonsComponent],
+  declarations: [ButtonsComponent, FlexboxComponent],
   imports: [
     CommonModule,
     MaterialModule,
